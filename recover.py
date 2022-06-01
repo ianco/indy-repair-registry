b86c7e793236defe17eedfb1446e82d8ed503ac4
Ian Costanzo <ian@anon-solutions.ca>
2022-06-01 09:58:30 -0700

    Use dynamic imports
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- recover.py
+++ recover.py
@@ -2,33 +2,30 @@
 
 import asyncio
 import hashlib
+import importlib
 import tempfile
 import time
 
 import aiohttp
 import base58
 
-from indy_credx import (
-    RevocationRegistry,
-    RevocationRegistryDefinition,
-    RevocationRegistryDelta,
-)
-from indy_vdr import open_pool, ledger
-
 
 async def fetch_txns(genesis_path, registry_id):
-    pool = await open_pool(genesis_path)
+    vdr_module = importlib.import_module(f"indy_vdr")
+    credx_module = importlib.import_module(f"indy_credx")
+
+    pool = await vdr_module.open_pool(genesis_path)
     print("Connected to pool")
 
     print("Fetch registry:", registry_id)
-    fetch = ledger.build_get_revoc_reg_def_request(None, registry_id)
+    fetch = vdr_module.ledger.build_get_revoc_reg_def_request(None, registry_id)
     result = await pool.submit_request(fetch)
     if not result["data"]:
         print("Registry definition not found")
         return
     data = result["data"]
     data["ver"] = "1.0"
-    defn = RevocationRegistryDefinition.load(data)
+    defn = credx_module.RevocationRegistryDefinition.load(data)
     print("Tails URL:", defn.tails_location)
 
     async with aiohttp.ClientSession() as session:
@@ -47,7 +44,7 @@ async def fetch_txns(genesis_path, registry_id):
         tails_temp.close()
 
     to_timestamp = int(time.time())
-    fetch = ledger.build_get_revoc_reg_delta_request(
+    fetch = vdr_module.ledger.build_get_revoc_reg_delta_request(
         None, registry_id, None, to_timestamp
     )
     result = await pool.submit_request(fetch)
@@ -57,8 +54,8 @@ async def fetch_txns(genesis_path, registry_id):
 
     accum_to = result["data"]["value"]["accum_to"]
     accum_to["ver"] = "1.0"
-    delta = RevocationRegistryDelta.load(accum_to)
-    registry = RevocationRegistry.load(accum_to)
+    delta = credx_module.RevocationRegistryDelta.load(accum_to)
+    registry = credx_module.RevocationRegistry.load(accum_to)
     print("Ledger registry state:", registry.to_json())
     revoked = set(result["data"]["value"]["revoked"])
     print("Ledger revoked indexes:", revoked)
@@ -97,6 +94,11 @@ def run(genesis_path, registry_id, set_revoked):
 if __name__ == "__main__":
     run(
         "genesis.txn",
-        "KJKW5xiNXeGHLhXeMwH1pk:4:KJKW5xiNXeGHLhXeMwH1pk:3:CL:39610:default:CL_ACCUM:ec631ced-b985-403f-bdad-e7f2da219660",
-        [1, 3, 5, 6],
+        "P6UvpU451XA5p8Dx6zsuUu:4:P6UvpU451XA5p8Dx6zsuUu:3:CL:38189:CredentialRevoke:CL_ACCUM:bf7cc241-5cf8-45e5-8aa8-891d0656c666",
+        [1, 3, 4, 5],
     )
+#    run(
+#        "genesis.txn",
+#        "KJKW5xiNXeGHLhXeMwH1pk:4:KJKW5xiNXeGHLhXeMwH1pk:3:CL:39610:default:CL_ACCUM:ec631ced-b985-403f-bdad-e7f2da219660",
+#        [1, 3, 5, 6],
+#    )
